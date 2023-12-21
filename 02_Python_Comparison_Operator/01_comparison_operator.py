ffe31a8792d9aedde1e7afce656aac342ca711f2
Bhupendra Singh <bhupi29491@gmail.com>
2023-12-21 23:32:06 +0200

    Added fundamentals of Operators in Python

---
+++ 02_Python_Comparison_Operator/01_comparison_operator.py
@@ -0,0 +1,35 @@
+# There are different types of comparison operator available in Python
+
+# == (Equality)
+# If the values of two operands are equal, then the condition becomes true.
+# print(2 == 2)   # O/P : True
+# print(2 == 1)   # O/P : False
+# print('hello' == 'bye')   # O/P : False
+# print('Bye' == 'bye')   # O/P : False
+# print(3.0 == 3)   # O/P : True
+
+
+# != (Not Equality)
+# If values of two operands are not equal, then condition becomes true.	(a != b) is true.
+# print(2 != 2)   # O/P : False
+# print(4 != 2)   # O/P : True
+
+# >	(Greater than)
+# If the value of left operand is greater than the value of right operand, then condition becomes true.
+# print(2 > 1)   # O/P : True
+# print(2 > 7)   # O/P : False
+
+# <	(Lesser than)
+# If the value of left operand is less than the value of right operand, then condition becomes true.
+# print(1 < 5)   # O/P : True
+# print(20 < 7)   # O/P : False
+
+# >=	(Greater than or Equal to)
+# If the value of left operand is greater than or equal to the value of right operand, then condition becomes true.
+# print(2 >= 2)   # O/P : True
+# print(2 >= 7)   # O/P : False
+
+# <	(Lesser than or Equal to)
+# If the value of left operand is less than or equal to the value of right operand, then condition becomes true.
+# print(1 <= 5)  # O/P : True
+# print(20 <= 7)  # O/P : False
---
+++ 02_Python_Comparison_Operator/02_chain_comparison_operator.py
@@ -0,0 +1,11 @@
+# and operator : All the conditions need to be true
+print((1 < 2) and 'Bhupi' == 'Bhupi')  # O/P : True
+print((1 < 2) and 'Bhupi' == 'Bhupss')  # O/P : False
+
+# or operator : Any one condition need to be true
+print((1 < 2) or 'Bhupi' == 'Bhupss')  # O/P : True
+print((100 == 2) or (2 == 2))  # O/P : True
+
+# not operator : All the conditions need to be true
+print(not (1 == 1))  # O/P : False
+print(not 400 > 500)  # O/P : True
