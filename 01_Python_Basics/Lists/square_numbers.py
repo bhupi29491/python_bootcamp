b996f78dfce3ef5206eb1a69a8be4537830bff3b
Bhupendra Singh <bhupi29491@gmail.com>
2026-01-21 22:02:52 +0200

    Looping element in the list using for loop

---
+++ 01_Python_Basics/Lists/square_numbers.py
@@ -0,0 +1,25 @@
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+
+print(squares)
+
+# In more concise way
+squares = []
+for value in range(1, 11):
+    squares.append(value ** 2)
+
+print(squares)
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+# List Comprehensions example
+squares = [value ** 2 for value in range(1, 11)]
+print(squares)
+
+cubes = [value ** 3 for value in range(1, 11)]
+print(cubes)
