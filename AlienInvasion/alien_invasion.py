ad74cd7ab82044f425cd1db73868bf1e56cbb8e5
Bhupendra Singh <bhupi29491@gmail.com>
2026-02-22 11:36:34 +0200

    settings module is introduced for complete project

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -9,7 +11,8 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
